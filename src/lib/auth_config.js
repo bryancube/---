9a90c83c1ad56f68bf8996627d276195da7ec2a8
bryancube <87297322+bryancube@users.noreply.github.com>
2025-07-06 12:36:20 +0900

    Delete src/lib/auth_config.js

--- src/lib/auth_config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  domain: "dev-1jtdt5a7qchyocr2.us.auth0.com", // 대시보드에서 직접 복사하여 붙여넣기
-  clientId: "Ahyu2I5AF5SCaAgt7f03zWFiQzXNpPdQ"             // 대시보드에서 직접 복사하여 붙여넣기
-};
-
-export default config;
